8e7c1b11c163b84041b67f18596388cfd9563c0d
Polina <polinazholudeva511256@gmail.com>
2023-02-04 16:40:10 +0300

    feat:task1 is ready for himework2

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int number = numberRequest("Введите трехзначное число: ");
+if (number/100>=1)
+{
+    int secondDigit = getSecondDigit(number);
+    Console.WriteLine ($"Вторая цифра чила {number} = {secondDigit}");
+}
+else
+{
+     Console.WriteLine ($"Число не трехзначное");
+}
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int getSecondDigit (int number)
+{
+    int secondDigit = (number%100)/10;
+    return secondDigit;
+}

cca147b2bbf4ec55cb7e7db961e603f762146561
Polina <polinazholudeva511256@gmail.com>
2023-02-04 17:43:02 +0300

    feat:task2 to homework2

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+//8985663532 
+Console.WriteLine("Введите число : ");
+string input = Console.ReadLine();
+validateNumber(input);
+printThirdCharacter(input);
+
+void validateNumber(string input)
+{
+    Convert.ToInt32(input);
+}
+
+void printThirdCharacter(string input)
+{
+    if (input.Length < 3) {
+        Console.WriteLine ("Третьей цифры нет.");
+        return;
+    }
+    Console.WriteLine ($"Третья цифра чила {input} = {input[2]}");
+}

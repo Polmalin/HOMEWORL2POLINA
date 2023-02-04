3ac2d609fc0a2e662743af21c89c5c9519757b15
Polina <polinazholudeva511256@gmail.com>
2023-02-04 18:04:54 +0300

    feat:task3 is ready for homework2

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+int dayofweek = numberRequest("Введите порядковое число дня недели: ");
+if (dayofweek <= 0 || dayofweek > 7)
+{    
+    Console.WriteLine ("Ошибка: Такого количества дней в неделе нет!");
+}
+else if (dayofweek > 5) 
+{
+    Console.WriteLine ("Ответ: Выходной день.");
+}
+else
+{
+    Console.WriteLine ("Ответ: Будний день.");
+}
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
